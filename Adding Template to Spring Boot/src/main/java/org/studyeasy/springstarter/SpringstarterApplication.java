99e97def2625d5bb8698f9b846372cf4d98b7207
Satyam Singh <145654294+satyamsingh2003@users.noreply.github.com>
2024-11-21 11:36:07 +0530

    Update SpringstarterApplication.java

--- Adding Template to Spring Boot/src/main/java/org/studyeasy/springstarter/SpringstarterApplication.java
+++ Adding Template to Spring Boot/src/main/java/org/studyeasy/springstarter/SpringstarterApplication.java
@@ -9,5 +9,5 @@ public class SpringstarterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstarterApplication.class, args);
 	}
-
+// To start spring application
 }
